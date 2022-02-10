f8f4ad079c4e2dff630f60916d773829f92e6ae1
kbmgs <kbmgs00@gmail.com>
2022-02-10 15:05:18 +0800

    20220210-2

--- day02/src/com/atguigu/java/WindowTest2.java
+++ day02/src/com/atguigu/java/WindowTest2.java
@@ -6,6 +6,8 @@ package com.atguigu.java;
  * @description: 使用同步代码块解决继承Thread类的线程安全问题
  *               例子，创建三个窗口卖票。总票数为100张。使用继承Thread类的方式。
  *
+ *               说明：在继承Thread类创建多线程的方式中，要慎用this来充当同步监视器。考虑使用当前类来充当同步监视器。
+ *
  * @author kbmgs
  * @date 2022/2/2 17:31
  */
@@ -18,7 +20,9 @@ class Window2 extends Thread {
     public void run() {
         while (true) {
             //正确的
-            synchronized (obj) {
+//            synchronized (obj) {
+            //使用类来做监视器（类也是对象）
+            synchronized (Window2.class) {//Class clazz = Window2.class ，Window2.class只会加载一次，只有一个
                 //错误的方式：此时this代表t1,t2,t3三个对象
 //            synchronized (this) {
                 if (ticket > 0) {
